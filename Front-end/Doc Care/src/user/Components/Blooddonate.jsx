234d0f21c4984bd8564e75e0bac2993635a7b1fd
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-07-03 16:38:45 +0530

    Navbar

--- Front-end/Doc Care/src/user/Components/Blooddonate.jsx
+++ Front-end/Doc Care/src/user/Components/Blooddonate.jsx
@@ -1,52 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 function Blooddonate() {
     return (
         <>
-            {/* <div data-theme="light" className='h-screen'>
-                <h1 className='text-3xl font-bold'>PRODUCT ADDING</h1>
-                <form>
-                    <label>
-                        <div className="label">
-                            <span className="label-text font-bold">Enter Product Name</span>
-                        </div>
-                        <input required type="text" placeholder="Product Name" className="input input-bordered w-full max-w-xs" onChange={e => settitle(e.target.value)} />
-                        <div className="label">
-                        </div>
-                    </label>
-
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text font-bold">Upload Product Image</span>
-                        </div>
-                        <input required type="file" placeholder="Product URL" className="input input-bordered w-full max-w-xs" onChange={e => setimage(e.target.files[0])} />
-                        <div className="label">
-                        </div>
-                    </label>
-
-                   
-
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text font-bold">Enter Product Description</span>
-                        </div>
-                        <input required type="text" placeholder="Product Description" className="input input-bordered w-full max-w-xs" onChange={e => setdescription(e.target.value)} />
-                        <div className="label">
-                        </div>
-                    </label>
-
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text font-bold">Enter Product Price</span>
-                        </div>
-                        <input required type="text" placeholder="Product Price" className="input input-bordered w-full max-w-xs" onChange={e => setprice(e.target.value)} />
-                        <div className="label">
-                        </div>
-                    </label>
-                    <button type="submit" className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Submit</button>
-                </form>
-
-            </div > */}
+            <Navbar />
 
             <div className="bg-white bg-opacity-40 p-8 rounded-lg shadow-lg w-full max-w-sm mx-4">
                 <h2 style={{ fontFamily: 'inria-serif' }} className="text-3xl font-bold mb-6 text-center">Register</h2>
@@ -67,23 +25,23 @@ function Blooddonate() {
                         </label>
                     </div>
                     <div className="mb-4">
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text font-bold">Select Blood group</span>
-                        </div>
-                        <select className="h-[40px] w-full" onChange={e => setcategory(e.target.value)}>
-                            <option>Select category</option>
-                            <option value="A+">A+</option>
-                            <option value="A-">A-</option>
-                            <option value="B+">B+</option>
-                            <option value="B-">B-</option>
-                            <option value="AB+">AB+</option>
-                            <option value="AB-">AB-</option>
-                            <option value="O+">O+</option>
-                            <option value="O-">O-</option>
-                        </select>
-                    </label>
-</div>
+                        <label className="form-control w-full max-w-xs">
+                            <div className="label">
+                                <span className="font-bold">Select Blood group</span>
+                            </div>
+                            <select className="h-[40px] w-full" onChange={e => setcategory(e.target.value)}>
+                                <option>Select category</option>
+                                <option value="A+">A+</option>
+                                <option value="A-">A-</option>
+                                <option value="B+">B+</option>
+                                <option value="B-">B-</option>
+                                <option value="AB+">AB+</option>
+                                <option value="AB-">AB-</option>
+                                <option value="O+">O+</option>
+                                <option value="O-">O-</option>
+                            </select>
+                        </label>
+                    </div>
                     <div className="mb-4">
                         <label>
                             <div className="label">
@@ -126,4 +84,4 @@ function Blooddonate() {
     )
 }
 
-export default Blooddonate
+export default Blooddonate;
