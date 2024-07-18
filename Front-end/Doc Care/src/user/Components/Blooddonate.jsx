831b00429cc9699e07c8a4d963e1a59deadd85f1
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-07-18 16:51:18 +0530

    registration

--- Front-end/Doc Care/src/user/Components/Blooddonate.jsx
+++ Front-end/Doc Care/src/user/Components/Blooddonate.jsx
@@ -34,7 +34,6 @@ function Blooddonate() {
                 toast.success(result.data.message)
                 navigate('/')
             }).catch((error) => {
-                // console.log(error.response.data.Details.details);
                 toast.error(error.response.data.message)
             })
     };
@@ -43,161 +42,165 @@ function Blooddonate() {
             <Navbar />
 
             <div className="bg-red-50 bg-opacity-90 p-8 rounded-lg shadow-lg w-full max-w-6xl mx-auto my-8 flex flex-col md:flex-row">
-    <div className="w-full md:w-1/2 p-12 mt-[-30px] flex items-center justify-center">
-        <img src="https://static.vecteezy.com/system/resources/previews/008/190/860/original/human-blood-donate-on-white-background-free-vector.jpg" alt="Blood Donation" className="rounded-lg shadow-lg w-full" />
-    </div>
-    <div className="w-full md:w-1/2 p-4">
-        <h2 style={{ fontFamily: 'Inria Serif, serif' }} className="text-3xl font-bold mb-2 text-left text-red-600">Donor Registration</h2>
-        <h6 style={{ fontFamily: 'Inria Serif, serif' }} className="text-md mb-6 text-left text-gray-700">Donating blood is an act of solidarity. Join the effort and save lives.</h6>
-        <form onSubmit={bloodRegistration}>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div className="mb-4">
-                    <label>
-                        <div className="label text-left">
-                            <span className="font-bold">Name:</span>
-                        </div>
-                        <input
-                            type="text"
-                            id="Name"
-                            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
-                            placeholder="Donor name"
-                            required
-                            value={formData.Name}
-                            onChange={handleInputChange}
-                        />
-                    </label>
+                <div className="w-full md:w-1/2 p-12 mt-[-30px] flex items-center justify-center">
+                    <img src="https://static.vecteezy.com/system/resources/previews/008/190/860/original/human-blood-donate-on-white-background-free-vector.jpg" alt="Blood Donation" className="rounded-lg shadow-lg w-full" />
                 </div>
-                <div className="mb-4">
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label text-left">
-                            <span className="font-bold">Blood group:</span>
-                        </div>
-                        <select className="h-[40px] w-full border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
-                            id='Blood_group'
-                            required
-                            value={formData.Blood_group}
-                            onChange={handleInputChange}
-                        >
-                            <option>Select blood group</option>
-                            {bloodGroups.map((blood, index) => (
-                                <option key={index} value={blood}>{blood}</option>
-                            ))}
-                        </select>
-                    </label>
-                </div>
+                <div className="w-full md:w-1/2 p-4">
+                    <h2 style={{ fontFamily: 'Inria Serif, serif' }} className="text-3xl font-bold mb-2 text-left text-red-600">Donor Registration</h2>
+                    <h6 style={{ fontFamily: 'Inria Serif, serif' }} className="text-md mb-6 text-left text-gray-700">Donating blood is an act of solidarity. Join the effort and save lives.</h6>
+                    <form onSubmit={bloodRegistration}>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                            <div className="mb-4">
+                                <label>
+                                    <div className="label text-left">
+                                        <span className="font-bold">Name:</span>
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="Name"
+                                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
+                                        placeholder="Donor name"
+                                        required
+                                        value={formData.Name}
+                                        onChange={handleInputChange}
+                                    />
+                                </label>
+                            </div>
+                            <div className="mb-4">
+                                <label className="form-control w-full max-w-xs">
+                                    <div className="label text-left">
+                                        <span className="font-bold">Blood group:</span>
+                                    </div>
+                                    <select className="h-[40px] w-full border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
+                                        id='Blood_group'
+                                        required
+                                        value={formData.Blood_group}
+                                        onChange={handleInputChange}
+                                    >
+                                        <option>Select blood group</option>
+                                        {bloodGroups.map((blood, index) => (
+                                            <option key={index} value={blood}>{blood}</option>
+                                        ))}
+                                    </select>
+                                </label>
+                            </div>
 
-                <div className="mb-4">
-                    <label className="form-control w-full max-w-xs" htmlFor='Gender'>
-                        <div className="label text-left">
-                            <span className="font-bold">Gender:</span>
-                        </div>
-                        <select className="h-[40px] w-full border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
-                            id='Gender'
-                            required
-                            value={formData.Gender}
-                            onChange={handleInputChange}
-                        >
-                            <option>Select Gender</option>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                        </select>
-                    </label>
-                </div>
+                            <div className="mb-4">
+                                <label className="form-control w-full max-w-xs" htmlFor='Gender'>
+                                    <div className="label text-left">
+                                        <span className="font-bold">Gender:</span>
+                                    </div>
+                                    <select className="h-[40px] w-full border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
+                                        id='Gender'
+                                        required
+                                        value={formData.Gender}
+                                        onChange={handleInputChange}
+                                    >
+                                        <option>Select Gender</option>
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                    </select>
+                                </label>
+                            </div>
 
-                <div className="mb-4">
-                    <label>
-                        <div className="label text-left">
-                            <span className="font-bold">Age:</span>
+                            <div className="mb-4">
+                                <label>
+                                    <div className="label text-left">
+                                        <span className="font-bold">Age:</span>
+                                    </div>
+                                    <input
+                                        type="tel"
+                                        id="Age"
+                                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
+                                        placeholder="Age"
+                                        required
+                                        value={formData.Age}
+                                        onChange={handleInputChange}
+                                    />
+                                </label>
+                            </div>
+                            <div className="mb-4">
+                                <label>
+                                    <div className="label text-left">
+                                        <span className="font-bold">E-mail:</span>
+                                    </div>
+                                    <input
+                                        type="email"
+                                        id="Email"
+                                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
+                                        placeholder="E-mail"
+                                        required
+                                        value={formData.Email}
+                                        onChange={handleInputChange}
+                                    />
+                                </label>
+                            </div>
+                            <div className="mb-4">
+                                <label>
+                                    <div className="label text-left">
+                                        <span className="font-bold">Mobile Number:</span>
+                                    </div>
+                                    <input
+                                        type="tel"
+                                        id="Phone_number"
+                                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
+                                        placeholder="Phone number"
+                                        required
+                                        value={formData.Phone_number}
+                                        onChange={handleInputChange}
+                                    />
+                                </label>
+                            </div>
+                            <div className="mb-4">
+                                <label className="form-control w-full max-w-xs" htmlFor='District'>
+                                    <div className="label text-left">
+                                        <span className="font-bold">District:</span>
+                                    </div>
+                                    <select className="h-[40px] w-full border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
+                                        id='District'
+                                        required
+                                        value={formData.District}
+                                        onChange={handleInputChange}
+                                    >
+                                        <option>Select district</option>
+                                        {formData.State && districts[formData.State].map((district, index) => (
+                                            <option key={index} value={district}>{district}</option>
+                                        ))}
+                                    </select>
+                                </label>
+                            </div>
+                            <div className="mb-4">
+                                <label className="form-control w-full max-w-xs" htmlFor='State'>
+                                    <div className="label text-left">
+                                        <span className="font-bold">State:</span>
+                                    </div>
+                                    <select className="h-[40px] w-full border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
+                                        id='State'
+                                        required
+                                        value={formData.State}
+                                        onChange={handleInputChange}
+                                    >
+                                        <option value="">Select State</option>
+                                        {states.map((state, index) => (
+                                            <option key={index} value={state}>{state}</option>
+                                        ))}
+                                    </select>
+                                </label>
+                            </div>
                         </div>
-                        <input
-                            type="tel"
-                            id="Age"
-                            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
-                            placeholder="Age"
-                            required
-                            value={formData.Age}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                </div>
-                <div className="mb-4">
-                    <label>
-                        <div className="label text-left">
-                            <span className="font-bold">E-mail:</span>
-                        </div>
-                        <input
-                            type="email"
-                            id="Email"
-                            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
-                            placeholder="E-mail"
-                            required
-                            value={formData.Email}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                </div>
-                <div className="mb-4">
-                    <label>
-                        <div className="label text-left">
-                            <span className="font-bold">Mobile Number:</span>
-                        </div>
-                        <input
-                            type="tel"
-                            id="Phone_number"
-                            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
-                            placeholder="Phone number"
-                            required
-                            value={formData.Phone_number}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                </div>
-                <div className="mb-4">
-                    <label className="form-control w-full max-w-xs" htmlFor='District'>
-                        <div className="label text-left">
-                            <span className="font-bold">District:</span>
-                        </div>
-                        <select className="h-[40px] w-full border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
-                            id='District'
-                            required
-                            value={formData.District}
-                            onChange={handleInputChange}
+                        <button
+                            type="submit"
+                            className="w-full bg-red-600 text-white p-3 rounded-full hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-200 text-xl mt-[30px]"
                         >
-                            <option>Select district</option>
-                            {formData.State && districts[formData.State].map((district, index) => (
-                                <option key={index} value={district}>{district}</option>
-                            ))}
-                        </select>
-                    </label>
-                </div>
-                <div className="mb-4">
-                    <label className="form-control w-full max-w-xs" htmlFor='State'>
-                        <div className="label text-left">
-                            <span className="font-bold">State:</span>
-                        </div>
-                        <select className="h-[40px] w-full border border-gray-300 rounded focus:outline-none focus:ring focus:ring-red-200"
-                            id='State'
-                            required
-                            value={formData.State}
-                            onChange={handleInputChange}
-                        >
-                            <option value="">Select State</option>
-                            {states.map((state, index) => (
-                                <option key={index} value={state}>{state}</option>
-                            ))}
-                        </select>
-                    </label>
+                            Submit
+                        </button>
+                    </form>
+                    <div className="items-center justify-between mt-4">
+                        <a href="#" style={{ fontFamily: 'inria-serif' }} className="text-md text-black-600">Do you need blood? </a>
+                        <a href="#" style={{ fontFamily: 'inria-serif' }} className="text-md text-blue-600 hover:underline hover:text-blue-800 font-bold" onClick={() => navigate("/")}>Register</a>
+                    </div>
                 </div>
             </div>
-            <button
-                type="submit"
-                className="w-full bg-red-600 text-white p-3 rounded-full hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-200 text-xl mt-[30px]"
-            >
-                Submit
-            </button>
-        </form>
-    </div>
-</div>
 
 
         </>
