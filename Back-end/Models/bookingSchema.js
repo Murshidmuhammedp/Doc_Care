b8e0445e6e582a8c35b05da241eefb9e7068d847
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-07-26 19:10:13 +0530

    booking Schema

--- Back-end/Models/bookingSchema.js
+++ Back-end/Models/bookingSchema.js
@@ -6,9 +6,34 @@ const bookingSchema = new mongoose.Schema({
         ref: "Users",
         required: true
     },
-    doctorId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"doctors",
-        required:true
+    doctorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "doctors",
+        required: true
     },
-})
+    patient_name: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: Number,
+        required: true
+    },
+    time: [{
+
+    }],
+    bookingTime_Date: {
+        type: Date,
+        required: true,
+        default: () => {
+            const now = new Date();
+            now.setHours(now.getHours() + 5);
+            now.setMinutes(now.getMinutes() + 30);
+            return now;
+        }
+    },
+});
+
+const booking = mongoose.model('booking', bookingSchema);
+
+export default booking;
