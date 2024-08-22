6a72031feeac4f08975804f979479c061cde08b3
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-08-22 16:19:15 +0530

    vite confiq

--- Front-end/Doc Care/vite.config.js
+++ Front-end/Doc Care/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',
+    chunkSizeWarningLimit: 600, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; 
+          }
+        },
+      },
+    },
+  },
+});
+
