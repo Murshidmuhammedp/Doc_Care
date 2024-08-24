cdab4bb9c2870ef4efa7391bd09dcc397ba17394
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-08-24 10:35:51 +0530

    url update

--- Back-end/server.js
+++ Back-end/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(express.json());
 
 app.use(cors({
-    // origin: "http://localhost:5173",
+    origin: "http://localhost:5173",
     origin: "https://doc-care-eight.vercel.app/",
     credentials: true
 }));
--- Front-end/Doc Care/src/confiq/axios.js
+++ Front-end/Doc Care/src/confiq/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const customAxios=axios.create({
-    baseURL:"http://localhost:9876"
+    // baseURL:"http://localhost:9876",
+    baseURL:"https://doc-care-back-end.onrender.com"
 })
