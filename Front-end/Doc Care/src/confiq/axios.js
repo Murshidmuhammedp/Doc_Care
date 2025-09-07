55abae65b60ef3abb1454a465a3ea6d9c2778b54
Murshidmuhammedp <murshid.mmp@gmail.com>
2025-09-07 16:58:20 +0530

    axios

--- Front-end/Doc Care/src/confiq/axios.js
+++ Front-end/Doc Care/src/confiq/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const customAxios=axios.create({
-    baseURL:"http://localhost:9876",
-    // baseURL:"https://doc-care-back-end.onrender.com"
+    // baseURL:"http://localhost:9876",
+    baseURL:"https://doc-care-back-end.onrender.com"
 })
